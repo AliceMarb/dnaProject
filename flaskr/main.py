65a102274b788906a63f5d086c61ef00f7670582
Alice <alice.marbach@gmail.com>
2020-09-17 15:48:47 +0100

    added requirements

--- flaskr/main.py
+++ flaskr/main.py
@@ -24,4 +24,6 @@ def home():
     return render_template("template.html")
 
 # if __name__ == "__main__":
+#     # This is to set FLASK_ENV=development if this is being 
+#     # run using python app.py 
 #     app.run(debug=True)
